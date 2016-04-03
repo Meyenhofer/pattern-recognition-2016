2f010ff173e2abc25fe4d28ff4e21b1c645e474e
David Wettstein <dwettstein@gmail.com>
2016-04-03 16:03:55 +0200

    Added MLP files from current scikit-learn  master branch (not in version 0.17.1 yet). Run plot_mnist_filters.py as an example.

--- examples.py
+++ examples.py
@@ -5,24 +5,29 @@ from configparser import ConfigParser as ConfigParser
 from utils import fio
 from svm import test_svm
 
+def main():
+    config = ConfigParser()
+    config.read('config.ini')
+    print("Config sections: %s" % config.sections())
 
-config = ConfigParser()
-config.read('config.ini')
-print("Config sections: %s" % config.sections())
+    print("Reading MNIST data")
+    x, y = fio.parse_mnist(config.get('MNIST', 'testset'), 100)
+    print("   parsed %s lines" % x.shape[0])
 
-print("Reading MNIST data")
-x, y = fio.parse_mnist(config.get('MNIST', 'testset'), 100)
-print("   parsed %s lines" % x.shape[0])
-
-print("Get a plot path")
-pp = fio.plot_file("test")
-print("   " + pp)
-test_svm.run()
+    print("Get a plot path")
+    pp = fio.plot_file("test")
+    print("   " + pp)
+    test_svm.run()
 
 
-lables, data = fio.import_csv_data(config.get('MNIST', 'testset'))
-print("Lables length: %i" % len(lables))
-print("Data length: %i" % len(data))
-sample_data = fio.get_random_data_sample(data, 100)
-print("Sample data length: %i" % len(sample_data))
-print("Sample data type: %s" % type(sample_data))
+    lables, data = fio.import_csv_data(config.get('MNIST', 'testset'))
+    print("Lables length: %i" % len(lables))
+    print("Data length: %i" % len(data))
+    sample_data = fio.get_random_data_sample(data, 100)
+    print("Sample data length: %i" % len(sample_data))
+    print("Sample data type: %s" % type(sample_data))
+
+
+# Program entry point. Don't execute if imported.
+if __name__ == '__main__':
+    main()
