816ab2207716f27af2c8d80920bb15be875a9884
felix <nibelungen@gmx.ch>
2016-04-03 23:12:19 +0200

    murks

--- examples.py
+++ examples.py
@@ -1,32 +1,33 @@
-# aggregation of the tests of the different packages
-
-from configparser import ConfigParser as ConfigParser
-
 from utils import fio
-from svm import test_svm
+
 
 def main():
-    config = ConfigParser()
-    config.read('config.ini')
-    print("Config sections: %s" % config.sections())
+    """
+    collection of examples to use the different modules and functions
+    """
 
-    print("Reading MNIST data")
+    # Get the configurations
+    config = fio.get_config()
+    print('Config sections: %s' % config.sections())
+
+    # Read text data
+    print('Reading MNIST data')
     x, y = fio.parse_mnist(config.get('MNIST', 'testset'), 100)
-    print("   parsed %s lines" % x.shape[0])
+    print('   parsed %s lines' % x.shape[0])
 
-    print("Get a plot path")
-    pp = fio.plot_file("test")
-    print("   " + pp)
-    test_svm.run()
-
-
+    print('Read csv data')
     csv_data = fio.import_csv_data(config.get('MNIST', 'testset'))
-    lables, data = fio.split_labels_data(csv_data, 0)
-    print("Lables length: %i" % len(lables))
-    print("Data length: %i" % len(data))
+    labels, data = fio.split_labels_data(csv_data, 0)
+    print('   Labels length: %i' % len(labels))
+    print('   Data length: %i' % len(data))
     sample_data = fio.get_random_data_sample(data, 100)
-    print("Sample data length: %i" % len(sample_data))
-    print("Sample data type: %s" % type(sample_data))
+    print('   Sample data length: %i' % len(sample_data))
+    print('   Sample data type: %s' % type(sample_data))
+
+    # Get a path for a (internal) plot file
+    print('Get a plot path')
+    pp = fio.get_plot_file('test')
+    print('   ' + pp)
 
 
 # Program entry point. Don't execute if imported.
