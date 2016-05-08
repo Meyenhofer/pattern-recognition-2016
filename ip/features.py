60d6a0ecd9c8929a704dc6e42d3ee2f15649f4d9
Michael Jungo <michaeljungo92@gmail.com>
2016-05-08 11:26:55 +0200

    Compute black white transitions

--- ip/features.py
+++ ip/features.py
@@ -13,18 +13,16 @@ def compute_features(roi, window_width=1, step_size=3):
         scv = roi[:, x1:x2]
         # gradient
         gra = bin[0:-2] - bin[1:-1]
-        # black-white transitions
-        bwt = gra == -1
-        # white-black transitions
-        wbt = gra == 1
+        # number of black-white and white-black transitions
+        bwt, wbt = transitions(gra)
         # digits not on the contour
         dk = gra == 0
         # foreground fraction
         fgf = bin.sum() / len(bin)
 
         fv = [fgf,
-              bwt.sum(),
-              wbt.sum(),
+              bwt,
+              wbt,
               dk.sum(),
               moment(scv, moment=1)[0],
               moment(scv, moment=2)[0],
@@ -39,3 +37,23 @@ def compute_features(roi, window_width=1, step_size=3):
             break
 
     return f
+
+def transitions(bin_img):
+    """
+    Returns the number of black to white and white to black transitions.
+    The two numbers will only be different when the image does not end in the
+    same colour as it started.
+    """
+    black_white_count = 0
+    white_black_count = 0
+    current_state = bin_img[0][0]
+    for lis in bin_img:
+        for val in lis:
+            if val is not current_state:
+                current_state = val
+                if val:
+                    black_white_count += 1
+                else:
+                    white_black_count += 1
+
+    return black_white_count, white_black_count
