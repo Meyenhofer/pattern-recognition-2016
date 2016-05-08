efe2ff588a16be2e2786ae1371bbc7f63b6e7bdb
Michael Jungo <michaeljungo92@gmail.com>
2016-05-08 21:55:06 +0200

    Select images and words by id for visualization

--- search/html.py
+++ search/html.py
@@ -1,8 +1,11 @@
 # Create the html display of a given search
 # Use the image as background and overlay the svg
 # (remove all the paths in the svg that were not found and write the copy in a new file)
+import os
 from svg.path import parse_path
 from xml.dom.minidom import Document, parse, parseString
+from utils.fio import get_absolute_path, get_config
+from utils.transcription import WordCoord
 
 class HTMLVisualization:
     def __init__(self):
@@ -17,7 +20,8 @@ class HTMLVisualization:
         css.appendChild(text)
         self.head.appendChild(css)
 
-    def add_image(self, img_src, svg_src=None, svg_str=None, img_id=None, paths=None):
+    # TODO: Refactor!
+    def add_image(self, img_src, svg_src=None, svg_str=None, img_id=None, paths=None, words=None):
         div = self.__document.createElement('div')
         if img_id:
             div.setAttribute('id', img_id)
@@ -27,12 +31,23 @@ class HTMLVisualization:
         elif svg_str:
             svg_doc = parseString(svg_str)
         if svg_doc:
-            if paths is not None:
+            if words is not None:
+                svg_doc = filter_svg_by_id(svg_doc, words)
+                div.appendChild(svg_doc)
+            elif paths is not None:
                 svg_doc = filter_svg(svg_doc, paths)
                 div.appendChild(svg_doc)
             else:
                 div.appendChild(svg_doc.documentElement)
         self.body.appendChild(div)
+
+    def add_image_by_id(self, img_id, word_ids=None):
+        config = get_config()
+        img_dir = get_absolute_path(config.get('KWS', 'images'))
+        img_src = os.path.join(img_dir, img_id + '.jpg')
+        svg_dir = get_absolute_path(config.get('KWS', 'locations'))
+        svg_src = os.path.join(svg_dir, img_id + '.svg')
+        self.add_image(img_src, svg_src=svg_src, words=word_ids)
 
     def save(self, file_path='output.html'):
         file_handle = open(file_path, 'w')
@@ -40,7 +55,6 @@ class HTMLVisualization:
         file_handle.close()
 
 def filter_svg(svg_doc, paths):
-    print(svg_doc, svg_doc.getElementsByTagName('svg'))
     svg_root = svg_doc.getElementsByTagName('svg')[0]
     # copy only the root node
     new_svg = svg_root.cloneNode(False)
@@ -48,6 +62,18 @@ def filter_svg(svg_doc, paths):
         parsed_path = parse_path(element.getAttribute('d'))
         for path in paths:
             if path == parsed_path:
+                new_svg.appendChild(element)
+                break
+
+    return new_svg
+
+def filter_svg_by_id(svg_doc, word_ids):
+    svg_root = svg_doc.getElementsByTagName('svg')[0]
+    # copy only the root node
+    new_svg = svg_root.cloneNode(False)
+    for element in svg_doc.getElementsByTagName('path'):
+        for word_id in word_ids:
+            if element.getAttribute('id') == word_id:
                 new_svg.appendChild(element)
                 break
 
